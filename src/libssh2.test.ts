cbc3369db91a617eb2609ed34416c033c432d596
yskszk63 <yskszk63@gmail.com>
2022-04-06 20:50:23 +0900

    wip: add tests...

--- src/libssh2.test.ts
+++ src/libssh2.test.ts
@@ -13,46 +13,44 @@ let container: undefined | {
   ipaddr: string
 };
 
+async function docker(args: string[], capture: true): Promise<string>
+async function docker(args: string[], capture: false): Promise<void>
+async function docker(args: string[]): Promise<void>
+async function docker(args: string[], capture: boolean | undefined = false): Promise<string | void> {
+  const proc = spawn("docker", args, {
+    stdio: capture ? ["ignore", "pipe", "inherit"]: "ignore",
+  });
+
+  const buf: string[] = [];
+  if (capture) {
+    if (!proc.stdout) {
+      throw new Error(); // unreachable
+    }
+    const decoder = new TextDecoder();
+    proc.stdout.on("data", (chunk) => {
+      buf.push(decoder.decode(chunk, { stream: true }));
+    });
+  }
+
+  const result = await new Promise<number>((resolve, reject) => {
+    proc.on("exit", resolve);
+    proc.on("error", reject);
+  });
+  if (result) {
+    throw new Error(`failed ${result}: docker ${args.join(" ")}`);
+  }
+
+  if (!capture) {
+    return;
+  }
+  return buf.join("").trimEnd();
+}
+
 beforeAll(async () => {
   const dir = fileURLToPath(new URL("./libssh2.test/", import.meta.url));
-  {
-    const proc = spawn("docker", ["buildx", "build", dir, "-t", "libssh2-test-sshd"], { stdio: ["ignore", "inherit", "inherit"] });
-    const result = await new Promise<number>((resolve, reject) => {
-      proc.on("exit", resolve);
-      proc.on("error", reject);
-    });
-    if (result) {
-      throw new Error(`result: ${result}`);
-    }
-  }
-
-  let name = "";
-  {
-    const proc = spawn("docker", ["run", "--rm", "-d", "libssh2-test-sshd"], { stdio: ["ignore", "pipe", "inherit"] });
-    proc.stdout.on("data", (chunk: Buffer) => name += chunk.toString("utf8"));
-    const result = await new Promise<number>((resolve, reject) => {
-      proc.on("exit", resolve);
-      proc.on("error", reject);
-    });
-    if (result) {
-      throw new Error(`result: ${result}`);
-    }
-    name = name.trimEnd();
-  }
-
-  let ipaddr = "";
-  {
-    const proc = spawn("docker", ["inspect", "-f", "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}", name], { stdio: ["ignore", "pipe", "inherit"] });
-    proc.stdout.on("data", (chunk: Buffer) => ipaddr += chunk.toString("utf8"));
-    const result = await new Promise<number>((resolve, reject) => {
-      proc.on("exit", resolve);
-      proc.on("error", reject);
-    });
-    if (result) {
-      throw new Error(`result: ${result}`);
-    }
-    ipaddr = ipaddr.trimEnd();
-  }
+  await docker(["buildx", "build", dir, "-t", "libssh2-test-sshd"]);
+  const name = await docker(["run", "--rm", "-d", "libssh2-test-sshd"], true);
+  const ipaddr = await docker(["inspect", "-f", "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}", name], true);
 
   container = {
     id: name,
@@ -64,17 +62,7 @@ afterAll(async () => {
   if (typeof container === "undefined") {
     return;
   }
-
-  {
-    const proc = spawn("docker", ["kill", container.id], { stdio: ["ignore", "inherit", "inherit"] });
-    const result = await new Promise<number>((resolve, reject) => {
-      proc.on("exit", resolve);
-      proc.on("error", reject);
-    });
-    if (result) {
-      throw new Error(`result: ${result}`);
-    }
-  }
+  await docker(["kill", container.id]);
 });
 
 async function cc(src: string): Promise<Uint8Array> {
@@ -107,21 +95,38 @@ async function netFactory(host: string, port: number): Promise<[ReadableStream<U
   const sock = net.createConnection(port, host);
   sock.pause();
 
+  let release: (() => void) | undefined;
   const reader = new ReadableStream({
     start(controller) {
-      sock.on("data", data => {
+      sock.on("data", ondata);
+      sock.on("error", onerror);
+      sock.on("close", onclose);
+
+      release = () => {
+        sock.off("data", ondata);
+        sock.off("error", onerror);
+        sock.off("close", onclose);
+      }
+
+      function ondata(data: Uint8Array) {
         controller.enqueue(data);
         sock.pause();
-      });
-      sock.on("error", err => {
+      }
+      function onerror(err: unknown) {
         controller.error(err);
-      });
-      sock.on("close", () => {
+      }
+      function onclose() {
         controller.close();
-      });
+      }
     },
     pull() {
       sock.resume();
+    },
+    cancel() {
+      if (release) {
+        release();
+      }
+      release = void 0;
     },
     type: "bytes",
   });
@@ -190,11 +195,49 @@ describe("Session.connect", () => {
       username: "root",
       privatekey: () => fs.readFile(new URL("./libssh2.test/id_ed25519", import.meta.url)),
     });
-    try {
-    } finally {
-      await session.disconnect();
-      session.close(); // TODO
-      session.free(); // TODO
+    await session.disconnect();
+    session.close(); // TODO
+    session.free(); // TODO
+  });
+
+  test("host key mismatch", async () => {
+    if (!container) {
+      throw new Error();
     }
+
+    const lib = await newLibssh2({
+      fetcher,
+      netFactory,
+      crypto: webcrypto as any,
+      ReadableStream,
+      WritableStream: WritableStream as any,
+    });
+    const result = lib.connect({
+      host: container.ipaddr,
+      knownhost: `${container.ipaddr} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFMy8Wg7bxkeS/C56sIuN5KaXPwujbtv1I/R6ZYm27Km`,
+      username: "root",
+      privatekey: () => fs.readFile(new URL("./libssh2.test/id_ed25519", import.meta.url)),
+    });
+    expect(result).rejects.toThrowError("Knownhost key check mismatch");
+  });
+  test("host key unknown", async () => {
+    if (!container) {
+      throw new Error();
+    }
+
+    const lib = await newLibssh2({
+      fetcher,
+      netFactory,
+      crypto: webcrypto as any,
+      ReadableStream,
+      WritableStream: WritableStream as any,
+    });
+    const result = lib.connect({
+      host: container.ipaddr,
+      knownhost: `0.0.0.0 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINN5qBcVhQt0gPmGxgsxgt9429S74QH/LWGuHjVBPZ9p`,
+      username: "root",
+      privatekey: () => fs.readFile(new URL("./libssh2.test/id_ed25519", import.meta.url)),
+    });
+    expect(result).rejects.toThrowError("Knownhost key check notfound");
   });
 });
